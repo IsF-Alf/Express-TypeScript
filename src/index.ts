89ccfff50c86037eb337c5f0fd0e01c31a9138d9
Isaac Alfonso <ifa24991@gmail.com>
2025-05-21 02:42:51 -0300

    services

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import express from 'express';
-const app = express();
-app.use(express.json());
+import express from 'express'
+import diarysRouter from './routes/diaries'
 
-const PORT = 3000;
+const app = express()
+app.use(express.json())
+
+const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-    console.log('Funcionando ' + new Date().toLocaleDateString());
-    res.send('Pong!');
+  console.log('Funcionando ' + new Date().toLocaleDateString())
+  res.send('Pong!')
 })
 
+app.use('/api/diaries', diarysRouter)
+
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-});
+  console.log(`Server is running on ${PORT}`)
+})
--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,5 +1,15 @@
-import express from 'express';
+import express from 'express'
 
-const router = express.Router();
+const router = express.Router()
 
+router.get('/', (_req, res) => {
+  res.send('Fetching all diaries')
+})
 
+router.post('/', (_req, res) => {
+//   const { date, weather, visibility } = req.body;
+//  console.log(date, weather, visibility);
+  res.send('Saving a diary!')
+})
+
+export default router
